c322a908a3c6e91d66eda3a68c986652847a0493
Ger Roza <rozagerardo@gmail.com>
2023-04-14 13:07:47 -0300

    added global-api-exception-handling and spring-and-problem-details-spec lessons codebse

---
+++ global-api-exception-handling-end/src/main/java/com/baeldung/rws/web/dto/ProjectDto.java
@@ -0,0 +1,45 @@
+package com.baeldung.rws.web.dto;
+
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.baeldung.rws.domain.model.Project;
+
+public record ProjectDto( // @formatter:off
+
+    Long id,
+
+    String code,
+
+    String name,
+
+    String description,
+
+    Set<TaskDto> tasks) { // @formatter:on
+
+    public static class Mapper {
+        public static Project toModel(ProjectDto dto) {
+            if(dto == null)
+                return null;
+
+            Project model = new Project(dto.code(), dto.name(), dto.description());
+            if (!Objects.isNull(dto.id())) {
+                model.setId(dto.id());
+            }
+            // we won't allow creating or modifying Projects via a Task
+            return model;
+        }
+
+        public static ProjectDto toDto(Project model) {
+            if(model == null)
+                return null;
+            Set<TaskDto> tasks = model.getTasks()
+                .stream()
+                .map(TaskDto.Mapper::toDto)
+                .collect(Collectors.toSet());
+            ProjectDto dto = new ProjectDto(model.getId(), model.getCode(), model.getName(), model.getDescription(), tasks);
+            return dto;
+        }
+    }
+}
---
+++ global-api-exception-handling-start/src/main/java/com/baeldung/rws/web/dto/TaskDto.java
@@ -0,0 +1,51 @@
+package com.baeldung.rws.web.dto;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import com.baeldung.rws.domain.model.Project;
+import com.baeldung.rws.domain.model.Task;
+import com.baeldung.rws.domain.model.TaskStatus;
+
+public record TaskDto( // @formatter:off
+    Long id,
+
+    String uuid,
+
+    String name,
+
+    String description,
+
+    LocalDate dueDate,
+
+    TaskStatus status,
+
+    Long projectId,
+
+    WorkerDto assignee) { // @formatter:on
+
+    public static class Mapper {
+        public static Task toModel(TaskDto dto, Long requestedId) {
+            if(dto == null)
+                return null;
+            Project project = new Project();
+            project.setId(dto.projectId());
+
+            Task model = new Task(dto.name(), dto.description(), dto.dueDate(), project, dto.status(), WorkerDto.Mapper.toModel(dto.assignee()), dto.uuid());
+            if (!Objects.isNull(dto.id())) {
+                model.setId(dto.id());
+            }
+
+            // we won't allow creating or modifying Projects via a Task
+            return model;
+        }
+
+        public static TaskDto toDto(Task model) {
+            if(model == null)
+                return null;
+            TaskDto dto = new TaskDto(model.getId(), model.getUuid(), model.getName(), model.getDescription(), model.getDueDate(), model.getStatus(), model.getProject()
+                .getId(), WorkerDto.Mapper.toDto(model.getAssignee()));
+            return dto;
+        }
+    }
+}
