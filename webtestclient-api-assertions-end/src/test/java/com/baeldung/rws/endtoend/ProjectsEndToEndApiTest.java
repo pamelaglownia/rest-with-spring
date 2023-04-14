fbc58e485d06afa254e148e9e06ef571a3545749
Ger Roza <rozagerardo@gmail.com>
2023-04-14 13:15:40 -0300

    Consistency changes: replicated exception handler logic and fixed test

--- webtestclient-api-assertions-end/src/test/java/com/baeldung/rws/endtoend/ProjectsEndToEndApiTest.java
+++ webtestclient-api-assertions-end/src/test/java/com/baeldung/rws/endtoend/ProjectsEndToEndApiTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import com.baeldung.rws.web.dto.ProjectDto;
