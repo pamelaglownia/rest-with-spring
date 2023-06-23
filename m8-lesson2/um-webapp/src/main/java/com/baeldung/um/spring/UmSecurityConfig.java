4bd80034ac9c958d24af71934a45694f7bbf92f5
Ger Roza <rozagerardo@gmail.com>
2023-06-23 09:13:15 -0300

    Upgraded deprecated WebSecurityConfigurerAdapter usage

--- m8-lesson2/um-webapp/src/main/java/com/baeldung/um/spring/UmSecurityConfig.java
+++ m8-lesson2/um-webapp/src/main/java/com/baeldung/um/spring/UmSecurityConfig.java
@@ -1,17 +1,19 @@
 package com.baeldung.um.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 //@Configuration
-public class UmSecurityConfig extends WebSecurityConfigurerAdapter {
+public class UmSecurityConfig {
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .anyRequest()
             .permitAll();
+        return http.build();
     }
 
 }
