ee14c190d21fc08c9e0535888cfd24d1bc3b5501
Ger Roza <rozagerardo@gmail.com>
2023-06-23 09:43:01 -0300

    Split Module 2

--- the-basics-of-input-validation-start/src/main/java/com/baeldung/rws/persistence/repository/ProjectRepository.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.rws.persistence.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.baeldung.rws.domain.model.Project;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
